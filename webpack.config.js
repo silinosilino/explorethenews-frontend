3d6d215b448fa9da6cb39677f1640798818ed89a
Elena Silina <silina.prQgmail.com>
2020-05-01 20:42:32 -0700

    edited webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,31 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/index.js' },
+    entry: {
+        main: './src/index.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
-        },
-module: {
-    rules: [{
-        test: /\.js$/,
-        use: { loader: "babel-loader" },
-        exclude: /node_modules/
+    },
+    module: {
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
+    ]
 };
